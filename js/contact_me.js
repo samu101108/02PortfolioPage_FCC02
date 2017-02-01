64265652d78499381ee9a173d8b71fcb6830176d
Barbosa S. A <samu101108@gmail.com>
2017-02-01 00:19:00 -0300

    update photo profile and some words

--- js/contact_me.js
+++ js/contact_me.js
@@ -21,8 +21,8 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "http://samuel.net46.net/toExternal/contact_me.php",
-                //url: "././mail/contact_me.php",
+                url: "././mail/contact_me.php",
+                //url: "http://samuel.net46.net/toExternal/contact_me.php",
                 // samuel.net46.net/toExternal/contact_me.php
                 type: "POST",
                 data: {
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -20,7 +20,7 @@ $to = 'samu101108@gmail.com'; // Add your email address inbetween the '' replaci
 //yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: samu101108@gmail.com\n";//This is the email address the generated
+$headers = "From: noreply@samuel.net46.net\n";//This is the email address the generated
 //message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
